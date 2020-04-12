a7cde6f6d06fe08daeeafb17db8cd01de405560d
masego15 <masegom6@discovery.co.za>
2020-04-12 15:54:29 +0200

    Adding of fbProphet

--- 02/2.05.0/js/main.js
+++ 02/2.05.0/js/main.js
@@ -3,3 +3,43 @@
 *    Mastering Data Visualization with D3.js
 *    2.5 - Activity: Adding SVGs to the screen
 */
+
+var svg = d3.select("#chart-area").append("svg")
+				.attr("width", 500)
+				.attr("height", 400);
+
+
+svg.append("ellipse")
+	.attr("cx", 100)
+	.attr("cy", 250)
+	.attr("rx", 70)
+	.attr("ry",25)
+	.attr("fill", "grey");
+
+svg.append("ellipse")
+	.attr("cx", 300)
+	.attr("cy", 300)
+	.attr("rx", 50)
+	.attr("ry", 70)
+	.attr("fill", "yellow");
+
+svg.append("line")
+	.attr("x1", 185)
+	.attr("y1",5 )
+	.attr("x2", 230)
+	.attr("y2", 40)
+	.attr("stroke", "blue")
+	.attr("stroke-width", 5);	
+
+
+//x1="185" y1="5" x2="230" y2="40" stroke="blue" stroke-width="5"
+
+
+svg.append("rect")
+	.attr("x", 0)
+	.attr("y",0 )
+	.attr("width", 50)
+	.attr("height", 250)
+	.attr("fill", "green");
+
+//<rect x="0" y="0" width="50" height="50" fill="green"></rect>
--- 02/2.08.0/js/main.js
+++ 02/2.08.0/js/main.js
@@ -3,3 +3,27 @@
 *    Mastering Data Visualization with D3.js
 *    2.8 - Activity: Your first visualization!
 */
+
+    d3.json("data/buildings.json").then(function(data){
+    console.log(data); /* reading in data and looking at the data in the console*/
+
+    data.forEach(function(d) {
+        d.height = +d.height;
+    });
+
+    var rects = svg.selectAll("rect")
+            .data(data)
+        .enter().append("rect")
+            .attr("y", 0)
+            .attr("x", function(d, i){
+                return (i * 60);
+            })
+            .attr("width", 40)
+            .attr("height", function(d){
+                return d.height;
+            })
+            .attr("fill", function(d) {
+                return "grey";
+            });
+
+})
